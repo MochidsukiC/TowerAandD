c697926d0fc4998d10e17dcb456b35b4594773eb
BuildTools <unconfigured@null.spigotmc.org>
2024-05-03 11:29:43 +0900

    from Mochidsuki Main PC add SpawnPoint complete
    Ver.1.0.0 ALL COMPLETE

--- src/main/java/jp/houlab/mochidsuki/toweraandd/Listener.java
+++ src/main/java/jp/houlab/mochidsuki/toweraandd/Listener.java
@@ -2,6 +2,8 @@ package jp.houlab.mochidsuki.toweraandd;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -16,13 +18,13 @@ import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
 import org.codehaus.plexus.util.cli.shell.CmdShell;
 
 import java.util.Iterator;
 
-import static jp.houlab.mochidsuki.toweraandd.TowerAandD.config;
-import static jp.houlab.mochidsuki.toweraandd.TowerAandD.team1;
+import static jp.houlab.mochidsuki.toweraandd.TowerAandD.*;
 
 public class Listener implements org.bukkit.event.Listener {
     @EventHandler
@@ -44,16 +46,56 @@ public class Listener implements org.bukkit.event.Listener {
 
     @EventHandler
     public void PlayerRespawnEvent(PlayerRespawnEvent event){
-        Component textComponent = Component.text("[スポーン地点を選択]")
-                        .appendNewline().append(Component.text("[3]")).append(Component.text("   ")).append(Component.text("[4]"))
-                        .appendNewline().append(Component.text("   \\   /"))
-                        .appendNewline().append(Component.text("   ")).append(Component.text("[2]"))
-                        .appendNewline().append(Component.text("   /  \\"))
-                        .appendNewline().append(Component.text("[0]")).append(Component.text("   ")).append(Component.text("[1]"));
 
 
+        if(event.getPlayer().getScoreboard().getEntryTeam(event.getPlayer().getName()).getName() == V.siteStatus1.getTeam().getName()){
+            NamedTextColor[] colors = {NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED};
+            for(int i = 0;i<colors.length;i++) {
+                if (V.siteStatus1.getSpawn()[i]) {
+                    colors[i] = NamedTextColor.BLUE;
+                }
+            }
 
-        event.getPlayer().sendMessage(textComponent);
+
+            Component textComponent = Component.text("[200秒以内にスポーン地点を選択]")
+                    .appendNewline().append(Component.text("[4]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 4")).color(colors[4])).append(Component.text("   ")).append(Component.text("[3]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 3")).color(colors[3]))
+                    .appendNewline().append(Component.text("   \\   /"))
+                    .appendNewline().append(Component.text("   ")).append(Component.text("[2]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 2")).color(colors[2]))
+                    .appendNewline().append(Component.text("   /  \\"))
+                    .appendNewline().append(Component.text("[1]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 1")).color(colors[1])).append(Component.text("   ")).append(Component.text("[0]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 0")).color(colors[0]));
+            event.getPlayer().sendMessage(textComponent);
+            V.canUseSpawner.put(event.getPlayer(),true);
+            new BukkitRunnable(){
+
+                @Override
+                public void run() {
+                    V.canUseSpawner.put(event.getPlayer(),false);
+                }
+            }.runTaskLater(plugin,200);
+        } else if (event.getPlayer().getScoreboard().getEntryTeam(event.getPlayer().getName()).getName()== V.siteStatus2.getTeam().getName()) {
+            NamedTextColor[] colors = {NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED,NamedTextColor.RED};
+            for(int i = 0;i<colors.length;i++) {
+                if (V.siteStatus2.getSpawn()[i]) {
+                    colors[i] = NamedTextColor.BLUE;
+                }
+            }
+            Component textComponent = Component.text("[10秒以内にスポーン地点を選択]")
+                    .appendNewline().append(Component.text("[0]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 0")).color(colors[0])).append(Component.text("   ")).append(Component.text("[1]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 1")).color(colors[1]))
+                    .appendNewline().append(Component.text("   \\   /"))
+                    .appendNewline().append(Component.text("   ")).append(Component.text("[2]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 2")).color(colors[2]))
+                    .appendNewline().append(Component.text("   /  \\"))
+                    .appendNewline().append(Component.text("[3]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 3")).color(colors[3])).append(Component.text("   ")).append(Component.text("[4]").clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND,"/rsp 4")).color(colors[4]));
+            event.getPlayer().sendMessage(textComponent);
+            V.canUseSpawner.put(event.getPlayer(),true);
+            new BukkitRunnable(){
+
+                @Override
+                public void run() {
+                    V.canUseSpawner.put(event.getPlayer(),false);
+                }
+            }.runTaskLater(plugin,200);
+        }
+
     }
 
 
