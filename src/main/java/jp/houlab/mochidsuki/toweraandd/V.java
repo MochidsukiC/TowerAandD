50923f8629c924680dcead59520721dadf34d01e
soma <mrsumomo7@gmail.com>
2024-05-31 10:13:29 +0900

    from Mochidsuki MacBookAirM1 change V.java to public

--- src/main/java/jp/houlab/mochidsuki/toweraandd/V.java
+++ src/main/java/jp/houlab/mochidsuki/toweraandd/V.java
@@ -13,13 +13,13 @@ public static HashMap<Player,Integer> nextPlayerMoney;
 
 public static int Round;
 public static int RoundTimer;
-static BossBar TimerBossBar;
-static TeamStatus team1 = new TeamStatus();
-static TeamStatus team2 = new TeamStatus();
-static SiteStatus siteStatus1 = new SiteStatus();
-static SiteStatus siteStatus2 = new SiteStatus();
-static HashMap<Integer,Integer> spawnScore = new HashMap<>();
-static HashMap<Integer, ArmorStand> spawnTextDisplay = new HashMap<>();
-static HashMap<Integer, Location> spawnLocation = new HashMap<>();
-static HashMap<Player,Boolean> canUseSpawner = new HashMap<>();
+public static BossBar TimerBossBar;
+public static TeamStatus team1 = new TeamStatus();
+public static TeamStatus team2 = new TeamStatus();
+public static SiteStatus siteStatus1 = new SiteStatus();
+public static SiteStatus siteStatus2 = new SiteStatus();
+public static HashMap<Integer,Integer> spawnScore = new HashMap<>();
+public static HashMap<Integer, ArmorStand> spawnTextDisplay = new HashMap<>();
+public static HashMap<Integer, Location> spawnLocation = new HashMap<>();
+public static HashMap<Player,Boolean> canUseSpawner = new HashMap<>();
 }
